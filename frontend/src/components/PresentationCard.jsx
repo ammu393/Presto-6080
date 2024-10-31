78df0af4b45709747bd138ae288a5833826a17f4
Nicole Wang <z5479432@ad.unsw.edu.au>
2024-10-31 22:56:39 +1100

    fixed indentation

--- frontend/src/components/PresentationCard.jsx
+++ frontend/src/components/PresentationCard.jsx
@@ -1,12 +1,12 @@
 export function PresentationCard() {
-    return (
-        <div className="bg-white aspect-[2/1] max-w-lg flex flex-col p-4">
-            <div className="w-full h-3/4 bg-gray-300 mb-2 flex items-center justify-center">
-                <span className="text-sm text-gray-500">No Thumbnail</span>
-            </div>
-            <h3 className="text-lg font-semibold mb-1">Presentation Name</h3>
-            <p className="text-sm text-gray-600 mb-1">Description</p>
-            <span className="text-xs text-gray-500">Slides</span>
-        </div>
-    )
+  return (
+    <div className="bg-white aspect-[2/1] max-w-lg flex flex-col p-4">
+      <div className="w-full h-3/4 bg-gray-300 mb-2 flex items-center justify-center">
+        <span className="text-sm text-gray-500">No Thumbnail</span>
+      </div>
+      <h3 className="text-lg font-semibold mb-1">Presentation Name</h3>
+      <p className="text-sm text-gray-600 mb-1">Description</p>
+      <span className="text-xs text-gray-500">Slides</span>
+    </div>
+  )
 }
