2835f0101c0a6ccdc6cddc580ced1a4c86786d8c
Nicole Wang <z5479432@ad.unsw.edu.au>
2024-10-31 22:42:01 +1100

    made presentation title

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,16 @@
 import { DashboardHeader } from "../../components/DashboardHeader";
+import { PresentationCard } from "../../components/PresentationCard";
 
 export default function Dashboard() {
     return (
         <>
             <div className="flex flex-col h-screen">
                 <DashboardHeader />
-                <div className="bg-red"></div>
+                <div className="bg-[#f0f1f2] h-full">
+                    <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(100px,1fr))]">
+                        <PresentationCard />
+                    </div>
+                </div>
             </div>
         </>
     )
