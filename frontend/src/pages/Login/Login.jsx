f271c0c1e0ed51a371e3e53171e5d6eeac891a22
Arundhathi Madhu <z5420435@ad.unsw.edu.au>
2024-11-02 13:22:42 +1100

    building UI for login

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import NavBar from '../../NavBar';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    return (
+        <>
+        <div class="h-screen bg-register-background ">
+        <div class="flex justify-end items-center">
+        <a href="/register" class="bg-white my-3 mx-3 text-black hover:bg-grey py-2 px-4 rounded whitespace-nowrap">
+                Sign up 
+        </a> 
+        </div>
+        <div class="flex flex-col justify-center">
+        <div class="flex justify-center text-center">
+                <div>
+                    <p class="text-3xl font-normal text-[#828282]">
+                    Welcome back!
+                    </p>
+                    <p class="text-3lg text-[#ababab] mb-8">
+                    Let's get you signed in
+
+                    </p>
+                </div>
+            <br />
+            </div>
+            <div class="flex flex-col justify-center items-center">
+        <div class="bg-white w-[29rem] h-[35em] rounded shadow-lg">
+            <div class= "flex justify-center my-10">
+                through google/facebook login buttons here
+            </div>
+        <div class="inline-flex items-center justify-center w-full">
+            <hr class="w-64 h-px my-8 border-0 bg-gray-300" />
+            <span class="absolute px-3 font-medium text-gray-300 -translate-x-1/2 bg-white left-1/2 ">or</span>
+       </div>
+        <form class="max-w-md mx-auto flex flex-col justify-center ml-16">
+    <div class="relative mb-5 group">
+        <input
+            type="email"
+            name="floating_email"
+            id="floating_email"
+            class="block py-2 px-2 w-4/5 text-sm text-black border border-[#edeff2] bg-[#f7f8fa] appearance-none focus:outline-[#30b0e3] focus:bg-white focus:ring-0 peer placeholder-transparent"
+            placeholder=" "
+            required
+        />
+        <label
+            for="floating_email"
+            class="absolute text-sm px-2 text-gray-500 transition-transform duration-300 transform -translate-y-10 scale-100 top-3 left-0 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:scale-75 peer-focus:-translate-y-3.5 peer-valid:scale-75 peer-valid:-translate-y-6"
+        >
+            Email
+        </label>
+    </div>
+    <div class="relative z-0 w-full mb-5 group">
+        <input
+            type="password"
+            name="floating_password"
+            id="floating_password"
+            class="block py-2 px-2 w-4/5 text-sm text-black border border-[#edeff2] bg-[#f7f8fa] appearance-none focus:outline-[#30b0e3] focus:bg-white focus:ring-0 peer placeholder-transparent"
+            placeholder=" "
+            required
+        />
+        <label
+            for="floating_password"
+            class="absolute text-sm px-2 text-gray-500 transition-transform duration-300 transform -translate-y-2 scale-100 top-3 left-0 origin-[0] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:scale-75 peer-focus:-translate-y-3.5 peer-valid:scale-75 peer-valid:-translate-y-6"
+        >
+            Password
+        </label>
+    </div>
+</form>
+
+            </div>
+
+        </div>
+        </div>
+        </div>
+        </>
+
+    )
+}
