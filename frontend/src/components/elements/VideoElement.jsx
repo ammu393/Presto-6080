93b9ee93c56d204bf40f9b0e7f6d3c592296c43f
Nicole Wang <z5479432@ad.unsw.edu.au>
2024-11-12 21:16:26 +1100

    fix merge onflicts

--- frontend/src/components/elements/VideoElement.jsx
+++ frontend/src/components/elements/VideoElement.jsx
@@ -1,11 +1,10 @@
-
-
 export default function VideoElement({element, style}) {
+  const videoSrc = element.autoplay ? `${element.url}&autoplay=1` : element.url;
   return (
-    <video
-      src={element.url}
-      autoPlay={element.autoPlay}
-      controls
+    <iframe
+      src={videoSrc}
+      allowFullScreen
+      allow="autoplay; encrypted-media"
       style={{ width: "100%", height: "100%", objectFit: "cover", ...style }}
     />
   )
