76224eb8f597c1b3348ce1b838771b2d9aa1a3d6
Arundhathi Madhu <z5420435@ad.unsw.edu.au>
2024-11-12 13:46:23 +1100

    splitting commits pt3

--- frontend/src/pages/Presentation/Presentation.jsx
+++ frontend/src/pages/Presentation/Presentation.jsx
@@ -184,4 +184,96 @@ export default function Presentation({ token, store, setStore }) {
   
     await updatePresentationStore(updatedPresentation);
   };
+  
+  return (
+    <>
+      <div className="flex h-screen">
+        <button
+          onClick={toggleSidebar}
+          aria-controls="default-sidebar"
+          type="button"
+          className="fixed top-2 left-2 inline-flex items-center p-2 text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        >
+          <span className="sr-only">Open sidebar</span>
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+            />
+          </svg>
+        </button>
+
+        <PresentationSideBar token={token} store={store} setStore={setStore} isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} addElementToSlide={addElementToSlide} displaySlide={displaySlide} updateBackground={updateBackground}/>
+        <div className="flex-1 p-8 bg-gray-100 relative">
+          <button
+            onClick={toggleSidebar}
+            aria-controls="default-sidebar"
+            type="button"
+            className="inline-flex items-center p-2 text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+              />
+            </svg>
+          </button>
+
+          <div className="flex flex-row gap-6">
+            <h1 className="text-4xl">{getTitle()}</h1>
+            <img
+              src={editIcon}
+              alt="Edit Icon"
+              className="w-6 cursor-pointer transition-transform duration-200 hover:scale-110 hover:border"
+              tabIndex={0}
+              onClick={toggleModal}
+            />
+          </div>
+
+          <div className="aspect-Slide">
+            {displaySlide && (
+              <Slide 
+                displaySlide={displaySlide} 
+                slides = {slides}
+                addElementToSlide={addElementToSlide}
+                deleteElementFromSlide={deleteElementFromSlide}
+                presentation={presentation}
+              />
+            )}
+          </div>            
+          <div className='h-full flex flex-col absolute bottom-0 right-0 justify-center items-center pr-1  pb-5'>
+            <div className="h-8">
+              {/* Only display arrows if slides are present */}
+              <CreateButton setDisplaySlide={setDisplaySlide} token={token} store={store} setStore={setStore} presentationId={presentationId} setSlides={setSlides} updateURL={updateURL}/>
+              {slides.length > 0 && (
+                <div className="mt-5 h-8">
+                  <div className={isFirstSlide ? 'invisible' : ''}>
+                    <UpArrow onClick={moveSlideUp} />
+                  </div>
+                  <div className={isLastSlide ? 'invisible' : ''}>
+                    <DownArrow onClick={moveSlideDown} />
+                  </div>
+                  <DeleteButon setDisplaySlide={setDisplaySlide} token={token} store={store} setStore={setStore} presentationId={presentationId} displaySlide={displaySlide} setSlides={setSlides} updateURL={updateURL} className="mt-5"/>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {isModalOpen && (
+        <InputModal 
+          title="Edit Presentation Title"
+          placeholder="Enter New Title"
+          submitText="Submit"
+          isOpen={isModalOpen}
+          onClose={toggleModal}
+          onSubmit={handleTitleUpdate}
+        />
+      )}
+    </>
+  );
 }
